096a9b125bb304a02cb07bd16d1f8bd604f9b97c
Casey Primozic <me@ameo.link>
2016-07-10 01:59:50 -0500

    Added files from private repository

---
+++ server/src/tickrecorder/PrivateConfig.java
@@ -0,0 +1,9 @@
+package tickrecorder;
+
+public interface PrivateConfig {
+    public String FXCMUsername = "D102329430001";
+    public String FXCMPassword = "8934";
+    public String FXCMHostsURL = "http://www.fxcorporate.com/Hosts.jsp";
+    public String redisIP = "new.ameobea.me";
+    public int redisPort = 6379;
+}
